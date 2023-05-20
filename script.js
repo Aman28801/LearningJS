743b478ede915ada5f089fbb9353fc71ed9fa664
Aman <84791435+Aman28801@users.noreply.github.com>
2023-05-20 15:38:17 +0530

    loops

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ hello.addEventListener('click' , helloMsg);
 function helloMsg(){
     alert("Hello World");
 }
+// loops in array
+let animals = ["Aman","Amul","Akshat","Kunal"];
+
+for(let name of animals){
+    console.log(name)
+}
